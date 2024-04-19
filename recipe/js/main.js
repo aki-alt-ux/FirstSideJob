8716f5396b7702ad5650753a15ad741a43107b52
aki-alt-ux <ammcss0@gmail.com>
2024-04-19 22:18:52 +0900

    202404192220

--- recipe/js/main.js
+++ recipe/js/main.js
@@ -17,3 +17,13 @@ $(window).on('resize', function () {
   // ハンバーガーメニューを閉じる
   hamburger.removeClass('hamburger-menu-active');
 });
+
+// カルーセル
+$('.cover-slick').slick({
+  infinite: true,
+  dots: true,
+  arrows: true,
+  prevArrow: '<img src="images/cover/cover_arrow_left.svg" class="slide-arrow prev-arrow">',
+  nextArrow: '<img src="images/cover/cover_arrow_right.svg" class="slide-arrow next-arrow">',
+  fade: true
+});
