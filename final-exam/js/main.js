03f43670c6b3f2948c7ac73ad7a109de12dd51bc
aki-alt-ux <ammcss0@gmail.com>
2024-05-01 23:57:07 +0900

    202405012300

---
+++ final-exam/js/main.js
@@ -0,0 +1,9 @@
+// カルーセル
+$('.cover-slick').slick({
+  infinite: true,
+  dots: true,
+  arrows: true,
+  prevArrow: '<img src="images/cover/cover_arrow_left.svg" class="slide-arrow prev-arrow">',
+  nextArrow: '<img src="images/cover/cover_arrow_right.svg" class="slide-arrow next-arrow">',
+  fade: true
+});
