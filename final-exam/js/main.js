73c6ffdb2346bbfda9279359dc7541d5bb78d741
aki-alt-ux <ammcss0@gmail.com>
2024-05-04 06:53:25 +0900

    202405040700

--- final-exam/js/main.js
+++ final-exam/js/main.js
@@ -1,9 +1,38 @@
+var hamburger = $('.hamburger-menu');
+
+// ハンバーガーボタン
+//-------------------------------------------//
+// OPEN/CLOSEボタンをクリックしたら
+$('.hamburger-button').on('click', function () {
+  // console.log('click');
+
+  // .hamburgerの表示・非表示を繰り返す
+  hamburger.toggleClass('hamburger-menu-active');
+
+});
+
+// 画面幅のサイズが変わったら
+$(window).on('resize', function () {
+  // console.log('resize');
+
+  // ハンバーガーメニューを閉じる
+  hamburger.removeClass('hamburger-menu-active');
+});
+
+
+
+
 // カルーセル
 $('.cover-slick').slick({
   infinite: true,
   dots: true,
-  arrows: true,
-  prevArrow: '<img src="images/cover/cover_arrow_left.svg" class="slide-arrow prev-arrow">',
-  nextArrow: '<img src="images/cover/cover_arrow_right.svg" class="slide-arrow next-arrow">',
+  arrows: false,
+  autoplay: true,
+  autoplaySpeed: 3000,
   fade: true
+});
+
+// クリックでカルーセルを停止
+$('.cover-slick').on('click', function() {
+  $(this).slick('slickPause');
 });
