a5b0d29508bd1d8bb5f09f419f89682633a4ff58
aki-alt-ux <ammcss0@gmail.com>
2024-05-13 07:42:04 +0900

    202405130740

--- AAA/main.js
+++ AAA/main.js
@@ -44,7 +44,6 @@ $('#smarttab').smartTab({
 });
 
 //フェードイン
-//左から
 $('.js-fadein-left').waypoint({
   handler: function(direction) {
     // 要素が画面中央に来るとここが実行される
@@ -57,26 +56,6 @@ $('.js-fadein-left').waypoint({
        */
       $(this.element)
         .addClass('animate__fadeInLeft');
-    }
-  },
-
-  // 要素が画面中央に来たらhandlerを実行
-  offset: '50%',
-});
-
-//右から
-$('.js-fadein-right').waypoint({
-  handler: function(direction) {
-    // 要素が画面中央に来るとここが実行される
-    if (direction === 'down') {
-      /**
-       * 下方向のスクロール
-       * イベント発生元の要素に
-       * fadeInLeftアニメーションを付けることで
-       * アニメーションを開始
-       */
-      $(this.element)
-        .addClass('animate__fadeInRight');
     }
   },
 
